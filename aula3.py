d0eee48fba7a454991315bfe03acba4fa9eb12ba
Rafael <eng.omc@hotmail.com>
2025-05-08 22:04:18 -0300

    aula3

---
+++ aula3.py
@@ -0,0 +1,13 @@
+x = str(input("digite uma estrutura: "))
+
+if x == mt:
+    print('estrutura primaria')
+elif:
+    x == bt
+    print('estrutura secundaria')
+elif:
+    x == btmt
+    print('estrutura de primaria e secundaria')
+else:
+    print('nao reconheço esse estrutura')
+
